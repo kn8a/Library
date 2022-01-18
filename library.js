97b3ed5a7cc6e12d079e00756d46e4d9a71d6092
kn8a <mydreamhomefl@gmail.com>
2022-01-18 00:11:51 -0500

    base files

---
+++ library.js
@@ -0,0 +1,16 @@
+let myLibrary = [
+    {title:"book1", author:"author1", pagess:58, read:true},
+    {title:"book2", author:"author2", pagess:68, read:false},
+    {title:"book3", author:"author3", pagess:78, read:true},
+    {title:"book1", author:"author1", pagess:58, read:true},
+];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+    
+    myLibrary.push()
+}
+
